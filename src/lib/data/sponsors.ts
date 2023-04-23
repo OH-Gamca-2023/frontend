970bdc8d6c81b46c1113b32e4e8a9670624c8047
Jakub Šiagi <jakub.siagi@gmail.com>
2023-04-23 12:37:34 +0200

    Remove logos and sponsors

--- src/lib/data/sponsors.ts
+++ src/lib/data/sponsors.ts
@@ -5,31 +5,6 @@ export interface Sponsor {
 }
 
 export const sponsors: Sponsor[] = [
-	{
-		name: 'Bratislavský kraj',
-		logo: 'bk.svg',
-		url: 'https://bratislavskykraj.sk/',
-	},
-	{
-		name: 'Bratislava - Staré Mesto',
-		logo: 'b-sm.svg',
-		url: 'https://www.staremesto.sk/',
-	},
-	{
-		name: 'Mesto Bratislava',
-		logo: 'bratislava.svg',
-		url: 'https://www.bratislava.sk/',
-	},
-	{
-		name: 'Škubla & Partneri',
-		logo: 'skubla.svg',
-		url: 'https://www.skubla.sk/',
-	},
-	{
-		name: 'Stilus',
-		logo: 'stilus.svg',
-		url: 'https://www.stilus.sk/',
-	},
 ]
 
 export default sponsors
