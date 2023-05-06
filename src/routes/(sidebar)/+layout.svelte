c0c4c2d6d25ab6f2fcc377df57938c96a3b8c3e9
Jakub Šiagi <jakub.siagi@gmail.com>
2023-05-06 22:25:40 +0200

    Increase sidebar width

--- src/routes/(sidebar)/+layout.svelte
+++ src/routes/(sidebar)/+layout.svelte
@@ -5,12 +5,12 @@
 
 <div class="flex flex-col lg:flex-row w-full">
 	<div
-		class="flex flex-col items-center justify-center h-full basis-full lg:basis-4/6 xl:basis-3/4 my-3 md:mx-5 xl:mx-10"
+		class="flex flex-col items-center justify-center h-full basis-full lg:basis-2/3 2xl:basis-3/4 my-3 md:mx-5 xl:mx-8 2xl:mx-10"
 	>
 		<slot />
 	</div>
 	<div
-		class="flex flex-col items-center justify-center h-full basis-full lg:basis-2/6 xl:basis-1/4 my-3 md:mx-5 xl:mx-10"
+		class="flex flex-col items-center justify-center h-full basis-full lg:basis-1/3 2xl:basis-1/4 my-3 md:mx-5 xl:mx-8 2xl:mx-10"
 	>
 		<Pane>
 			<Sidebar />
