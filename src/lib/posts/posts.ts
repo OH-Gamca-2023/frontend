ccaa2cbad55d29313fd27860861cb89babc00e0a
Jakub Šiagi <jakub.siagi@gmail.com>
2023-05-12 11:14:20 +0200

    Make post author only partial

--- src/lib/posts/posts.ts
+++ src/lib/posts/posts.ts
@@ -1,8 +1,9 @@
 import { clazzes, grades } from '$lib/state'
-import type { Post, User } from '$lib/types'
+import type { Discipline, Post, User } from '$lib/types'
 import { PartialModel } from '$lib/utils/models'
 import { get } from 'svelte/store'
 import { categories, tags } from './data'
+import { disciplines } from './disciplines'
 
 class PostModel extends PartialModel<Post> {
 	constructor() {
@@ -11,17 +12,12 @@ class PostModel extends PartialModel<Post> {
 			(data) => {
 				const rawPost = data as any
 
-				const author = {
+				const author: Partial<User> = {
 					id: rawPost.author.id,
 					username: rawPost.author.username,
-					email: rawPost.author.email,
-					first_name: rawPost.author.first_name,
-					last_name: rawPost.author.last_name,
 					clazz: get(clazzes)[rawPost.author.clazz],
 					type: rawPost.author.type,
-					microsoft_user: 'hidden',
-					has_password: false,
-				} as User
+				}
 
 				rawPost.related_disciplines.forEach((id: any) => disciplines.load(id))
 
@@ -40,13 +36,14 @@ class PostModel extends PartialModel<Post> {
 					},
 
 					get discipline_categories() {
-						return this.related_disciplines.map((d) => d.category)
+						return this.related_disciplines.map((d: Discipline) => d.category)
 					},
-				} as Post
+				}
 			},
 			true,
 			[tags, categories, grades, clazzes],
-			'partial',
 		)
 	}
 }
+
+export const posts = new PostModel()
