27608690f6634d1a1490d661cf6632e46f1be938
Jakub Šiagi <jakub.siagi@gmail.com>
2023-05-27 20:20:43 +0200

    Allow api host override

--- src/lib/api/data.ts
+++ src/lib/api/data.ts
@@ -3,6 +3,11 @@ import { browser } from '$app/environment'
 export function getApiHost() {
 	if (!browser) return '/api'
 
+	if (browser) {
+		if (localStorage.getItem('api_host') !== null)
+			return localStorage.getItem('api_host') as string
+	}
+
 	if (window.location.port === '5173' || window.location.port === '4173')
 		return window.location.protocol + '//' + window.location.hostname + ':8000/api'
 	return window.location.protocol + '//' + window.location.hostname + '/api'
