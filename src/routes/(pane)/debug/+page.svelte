5cbe979470ab31bc1158e2f55190c65a85dcbdb9
Jakub Šiagi <jakub.siagi@gmail.com>
2023-06-15 09:35:53 +0200

    Add debug view

---
+++ src/routes/(pane)/debug/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { userState } from '$lib/state'
+</script>
+
+<div class="flex flex-col">
+	<span class="text-xl font-bold">Debug</span>
+	<span class="text-lg">State</span>
+	<pre class="text-sm">
+        {JSON.stringify($userState, null, 2)}
+    </pre>
+</div>
