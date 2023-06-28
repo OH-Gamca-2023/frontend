84e5c29c1f06a90cd9f186b5f80599fd556a5046
jaksia <jakub.siagi@gmail.com>
2023-06-28 17:23:13 +0200

    Improve filter parsing

--- src/routes/(sidebar)/(pane)/disciplines/[...filter]/+page.ts
+++ src/routes/(sidebar)/(pane)/disciplines/[...filter]/+page.ts
@@ -1,5 +1,8 @@
+import { disciplines } from '$lib/api/models'
 import type { FilterResult } from '$lib/components/filter/Filter.svelte'
+import { get } from 'svelte/store'
 import type { PageLoad } from './$types'
+import { redirect } from '@sveltejs/kit'
 
 export const prerender = false
 
@@ -9,35 +12,38 @@ export const load = (({params}) => {
         tags: [],
         grades: [],
     }
-    let requireResults = false
+    let rawFilter = params.filter
+    if(rawFilter.endsWith('/')) rawFilter = rawFilter.slice(0, -1)
+    if(rawFilter.startsWith('/')) rawFilter = rawFilter.slice(1)
+    const parts = rawFilter.split('/').filter(e => e.length > 0)
+
+
+    if(parts.length == 1) {
+        // Will work once disciplines are made cacheable
+        if (get(disciplines)[parts[0]] !== undefined) {
+            throw redirect(302, `/discipline/${parts[0]}`)
+        }
+    }
 
     let lastKey: string | undefined
-    if (params.filter) {
-        for (const part of params.filter.split('/')) {
-            if(part === 'news' || part === '') continue
-            if (lastKey) {
-                try {
-                    const ids = part.split(',').map(e => parseInt(e))
-                    if (ids.some(isNaN)) throw new Error('Invalid filter value for ' + lastKey)
-                    filter[lastKey as keyof FilterResult] = ids
-                } catch (e) {
-                    console.error(e)
-                }
-                lastKey = undefined
-            } else {
-                lastKey = part
-                if (lastKey === 'results') {
-                    requireResults = true, lastKey = undefined
-                    continue
-                }
-                if (![ 'categories', 'tags', 'grades' ].includes(lastKey)) console.error('Invalid filter key ' + lastKey)
+    for (const part of parts) {
+        if (lastKey) {
+            try {
+                const ids = part.split(',').map(e => parseInt(e))
+                if (ids.some(isNaN)) throw new Error('Invalid filter value for ' + lastKey)
+                filter[lastKey as keyof FilterResult] = ids
+            } catch (e) {
+                console.error(e)
             }
+            lastKey = undefined
+        } else {
+            lastKey = part
+            if (![ 'categories', 'grades' ].includes(lastKey)) console.error('Invalid filter key ' + lastKey)
         }
     }
 
     return {
         filter,
-        rawFilter: params.filter,
-        requireResults,
+        rawFilter,
     }
 }) satisfies PageLoad
--- src/routes/(sidebar)/(pane)/news/[...filter]/+page.ts
+++ src/routes/(sidebar)/(pane)/news/[...filter]/+page.ts
@@ -9,29 +9,31 @@ export const load = (({params}) => {
         tags: [],
         grades: [],
     }
+    let rawFilter = params.filter
+    if(rawFilter.endsWith('/')) rawFilter = rawFilter.slice(0, -1)
+    if(rawFilter.startsWith('/')) rawFilter = rawFilter.slice(1)
+    const parts = rawFilter.split('/').filter(e => e.length > 0)
 
     let lastKey: string | undefined
-    if (params.filter) {
-        for (const part of params.filter.split('/')) {
-            if(part === 'news' || part === '') continue
-            if (lastKey) {
-                try {
-                    const ids = part.split(',').map(e => parseInt(e))
-                    if (ids.some(isNaN)) throw new Error('Invalid filter value for ' + lastKey)
-                    filter[lastKey as keyof FilterResult] = ids
-                } catch (e) {
-                    console.error(e)
-                }
-                lastKey = undefined
-            } else {
-                lastKey = part
-                if (![ 'categories', 'tags', 'grades' ].includes(lastKey)) console.error('Invalid filter key ' + lastKey)
+    for (const part of parts) {
+        if(part === 'news' || part === '') continue
+        if (lastKey) {
+            try {
+                const ids = part.split(',').map(e => parseInt(e))
+                if (ids.some(isNaN)) throw new Error('Invalid filter value for ' + lastKey)
+                filter[lastKey as keyof FilterResult] = ids
+            } catch (e) {
+                console.error(e)
             }
+            lastKey = undefined
+        } else {
+            lastKey = part
+            if (![ 'categories', 'tags', 'grades' ].includes(lastKey)) console.error('Invalid filter key ' + lastKey)
         }
     }
 
     return {
         filter,
-        rawFilter: params.filter,
+        rawFilter
     }
 }) satisfies PageLoad
